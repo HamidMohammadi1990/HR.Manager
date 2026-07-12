bfac823e15f9a0afe36ceefcd9615d0fe12e4cd3
Hamid Mohammadnian <hamidmohammadnian@gmail.com>
2026-07-12 11:20:13 +0330

    remove test projects

--- EditionProject/tests/Edition.Infrastructure.Persistence.Tests/Fixtures/PersistenceTestCollection.cs
+++
@@ -1,7 +0,0 @@
-namespace JavidHrm.Infrastructure.Persistence.Tests.Fixtures;
-
-[CollectionDefinition(Name)]
-public sealed class PersistenceTestCollection : ICollectionFixture<SqlServerContainerFixture>
-{
-    public const string Name = "Persistence.SqlServer";
-}
