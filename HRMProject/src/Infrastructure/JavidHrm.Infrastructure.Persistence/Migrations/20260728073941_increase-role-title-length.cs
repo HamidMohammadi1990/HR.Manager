be319087f97fb12ec578c62af28ae3cc650f953b
HAMID-PC <40312758@PC-292>
2026-07-28 11:16:14 +0330

    increase roel title length

---
+++ HRMProject/src/Infrastructure/JavidHrm.Infrastructure.Persistence/Migrations/20260728073941_increase-role-title-length.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JavidHrm.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class increaseroletitlelength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Role",
+                type: "NVARCHAR(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(20)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Role",
+                type: "NVARCHAR(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)");
+        }
+    }
+}
